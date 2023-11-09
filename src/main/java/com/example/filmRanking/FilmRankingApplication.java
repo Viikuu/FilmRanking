5e4657e84d5378dfa3c9874a57535e17f732300d
Viikuu <viiktorw77@gmail.com>
2023-11-09 19:49:46 +0100

    feat: Init spring app

---
+++ src/main/java/com/example/filmRanking/FilmRankingApplication.java
@@ -0,0 +1,13 @@
+package com.example.filmRanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmRankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmRankingApplication.class, args);
+	}
+
+}
