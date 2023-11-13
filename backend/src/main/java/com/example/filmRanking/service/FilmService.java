33ed559643ae90a13afe80338420e519cc927ae7
Viikuu <viiktorw77@gmail.com>
2023-11-13 15:53:05 +0100

    feat: create Film repository and service

---
+++ backend/src/main/java/com/example/filmRanking/service/FilmService.java
@@ -0,0 +1,18 @@
+package com.example.filmRanking.service;
+
+import com.example.filmRanking.domain.FilmEntity;
+import java.util.List;
+import java.util.Optional;
+
+public interface FilmService {
+
+    FilmEntity createFilm(FilmEntity film);
+
+    Optional<FilmEntity> getFilmById(Long id);
+
+    List<FilmEntity> getAllFilms();
+
+    FilmEntity updateFilm(Long id, FilmEntity filmDetails);
+
+    void deleteFilm(Long id);
+}
