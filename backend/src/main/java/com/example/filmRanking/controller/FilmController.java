f5c7c77b763ac3a3450fdccbb7861188d28c8870
Viikuu <viiktorw77@gmail.com>
2023-11-13 17:11:34 +0100

    feat: create Film controller

---
+++ backend/src/main/java/com/example/filmRanking/controller/FilmController.java
@@ -0,0 +1,54 @@
+package com.example.filmRanking.controller;
+
+import com.example.filmRanking.domain.FilmEntity;
+import com.example.filmRanking.service.FilmService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/film")
+public class FilmController {
+
+    private final FilmService filmService;
+
+    @Autowired
+    public FilmController(FilmService filmService) {
+        this.filmService = filmService;
+    }
+
+    @GetMapping
+    public List<FilmEntity> getAllFilms() {
+        return filmService.getAllFilms();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<FilmEntity> getFilmById(@PathVariable Long id) {
+        return filmService.getFilmById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public FilmEntity addFilm(@RequestBody FilmEntity film) {
+        return filmService.createFilm(film);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<FilmEntity> updateFilm(@PathVariable Long id, @RequestBody FilmEntity filmDetails) {
+        try {
+            FilmEntity updatedFilm = filmService.updateFilm(id, filmDetails);
+            return ResponseEntity.ok(updatedFilm);
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteFilm(@PathVariable Long id) {
+        filmService.deleteFilm(id);
+        return ResponseEntity.ok().build();
+    }
+}
